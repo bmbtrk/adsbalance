28f289cde6825fda871d69246c460783e5a89473
bmbtrk <78149417+bmbtrk@users.noreply.github.com>
2023-01-19 16:12:35 +0300

    Add files via upload

---
+++ translate.js
@@ -0,0 +1,300 @@
+var langs = {
+    en: {
+        textTitle1: 'These women are ONLY looking for',
+        textTitle2: 'long-term relationships',
+        textQ1: 'Are you over 20 years?',
+        textQ2: 'Do you think finding a dating partner can be a good experience?',
+        textQ3: 'Would you like to meet a serious girl and spend good time together on our site?',
+        textThank: 'Thank you!',
+        textThankAfter: 'You can now see our list of women who are in your area and you can see their pictures. We ask you again please keep dems identity secret.',
+        textBtnk: 'Let me in!'
+    },
+    ar: {
+        textTitle1: 'هؤلاء النساء يبحثن فقط',
+        textTitle2: 'عن علاقات طويلة الأجل',
+        textQ1: 'هل يتجاوز عمرك 20 سنة؟',
+        textQ2: 'هل تعتقد أن العثور على شريك المواعدة يمكن أن يكون تجربة جيدة؟',
+        textQ3: 'هل ترغب في مقابلة فتاة جادة وقضاء وقت جيد معها على موقعنا؟',
+        textThank: 'شكراًشكرا لك!',
+        textThankAfter: 'يمكنك الآن مشاهدة قائمة النساء في منطقتك ويمكنك رؤية صورهن. نطلب منك مرة أخرى ، يرجى الحفاظ على سرية هوية dems',
+        textBtnk: 'دعني ادخل!'
+    },
+    tr: {
+        textTitle1: 'BU KADINLAR SADECE ',
+        textTitle2: 'UZUN DÖNEMLİ İLİŞKİ ARIYORLAR',
+        textQ1: '20 YAŞINDAN BÜYÜK MÜSÜNÜZ?',
+        textQ2: 'FLÖRT PARTNERİ BULMANIN İYİ BİR DENEYİM OLACAĞINI DÜŞÜNÜYOR MUSUNUZ?',
+        textQ3: 'SİTEMİZDEN CİDDİ BİR KADIN İLE TANIŞMAK VE İYİ ZAMAN GEÇİRMEK İSTER MİSİNİZ?',
+        textThank: 'Teşekkür ederim!',
+        textThankAfter: 'Artık listemiz ve bölgenizdeki kadınların fotoğraflarını görebilirsiniz. Yine, lütfen kimliklerini bir sır olarak saklayın.',
+        textBtnk: 'Girmeme izin ver!'
+    },
+    pt: {
+        textTitle1: 'ESTAS MULHERES ESTÃO À PROCURA APENAS DE',
+        textTitle2: 'RELACIONAMENTOS DE LONGO-PRAZO',
+        textQ1: 'TEM MAIS DE 20 ANOS?',
+        textQ2: 'ACHA QUE ENCONTRAR UM PARCEIRO DE ENCONTROS PODE SER UMA BOA EXPERIÊNCIA?',
+        textQ3: 'GOSTARIA DE CONHECER UMA RAPARIGA SÉRIA E PASSAR UM BOM TEMPO JUNTOS NO NOSSO SITE?',
+        textThank: 'Obrigado!',
+        textThankAfter: 'Já podes ver a nossa lista e fotos de mulheres na tua zona. Novamente, por favor mantém a sua identidade secreta.',
+        textBtnk: 'Deixe-me entrar!'
+    },
+    th: {
+        textTitle1: 'ผู้หญิงเหล่านี้กำลังมองหา',
+        textTitle2: 'ความสัมพันธ์ระยะยาวเท่านั้น',
+        textQ1: 'คุณอายุเกิน 20 ปีหรือไม่?',
+        textQ2: 'คุณคิดว่าการหาคู่เดทจะเป็นประสบการณ์ที่ดีไหม?',
+        textQ3: 'คุณต้องการเจอผู้หญิงที่จริงจังและอยากจะใช้เวลาที่ดีร่วมกันบนเว็บไซต์ของเราไหม?',
+        textThank: 'ขอบคุณ!',
+        textThankAfter: 'ตอนนี้คุณสามารถดูรายชื่อผู้หญิงที่อยู่ในพื้นที่ของคุณและคุณสามารถดูรูปภาพของพวกเขาได้. อีกครั้งโปรดเก็บข้อมูลเป็นความลับ',
+        textBtnk: 'ให้ฉันเข้าไป!'
+    },
+    de: {
+        textTitle1: 'DIESE FRAUEN SUCHEN NUR NACH',
+        textTitle2: 'LANGFRISTIGE BEZIEHUNGEN',
+        textQ1: 'SIND SIE ÜBER 20 JAHRE?',
+        textQ2: 'DENKEN SIE, DASS ES EINE GUTE ERFAHRUNG SEIN KÖNNTE EINEN DATING-PARTNER ZU FINDEN?',
+        textQ3: 'MÖCHTEN SIE EIN MÄDCHEN TREFFEN, DAS ES ERNST MEINT UND ZUSAMMEN EINE GUTE ZEIT AUF UNSERER WEBSITE VERBRINGEN?',
+        textThank: 'Vielen Dank!',
+        textThankAfter: 'Sie können jetzt auf unsere Liste und Fotos von Frauen zugreifen, die in Ihrer Nähe sind. Bitte halten Sie ihre Identität geheim.',
+        textBtnk: 'Lassen Sie mich rein!'
+    },
+    da: {
+        textTitle1: 'DISSE KVINDER ER KUN PÅ UDKIG EFTER',
+        textTitle2: 'LANGVARIGE FORHOLD',
+        textQ1: 'ER DU OVER 20 ÅR?',
+        textQ2: 'TÆNKER DU, AT DET AT FINDE EN DATINGSPARTNER KAN VÆRE EN GOD OPLEVELSE?',
+        textQ3: 'VIL DU GERNE MØDE EN SERIØS PIGEE OG HAVE EN GOD TID SAMMEN PÅ VORES WEBSTED?',
+        textThank: 'Tak skal du have!',
+        textThankAfter: 'Du kan nu se vores liste over kvinder, der er i dit område, og du kan se deres billeder. Vi beder dig igen, hold dems identitet hemmelig.',
+        textBtnk: 'Luk mig ind!'
+    },
+    sv: {
+        textTitle1: 'DESSA KVINNOR LETAR ENDAST EFTER',
+        textTitle2: 'LÅNGVARIGA FÖRHÅLLANDEN',
+        textQ1: 'ÄR DU ÖVER 20 ÅR GAMMAL',
+        textQ2: 'TROR DU PÅ ATT DET KAN VARA EN BRA UPPLEVELSE ATT HITTA EN DEJTINGPARTNER?',
+        textQ3: 'SKULLE DU VILJA TRÄFFA EN SERIÖS TJEJ OCH TILLBRINGA TID MED VARANDRA PÅ VÅR WEBBPLATS?',
+        textThank: 'Tack!',
+        textThankAfter: 'Du kan nu se vår lista och foton på kvinnor i ditt område. Vi ber dig igen, håll dems identitet hemlig.',
+        textBtnk: 'Släpp in mig!'
+    },
+    ja: {
+        textTitle1: 'この女性たちは長いおつきあいをできる人のみを探しています。',
+        textTitle2: '',
+        textQ1: 'あなたは20歳以上ですか？',
+        textQ2: 'デートの相手を探すのはよい経験になると思いますか？',
+        textQ3: '私たちのサイトで真剣な交際ができる女の子を見つけて素敵な時間を過ごすのは、いかがですか？',
+        textThank: 'ありがとうございます！',
+        textThankAfter: 'あなたの地域にいる女性のリストと写真を見ることができます。女性の身元情報に関しては、秘密にしてください。',
+        textBtnk: '参加させてください！'
+    },
+    id: {
+        textTitle1: 'PARA WANITA INI HANYA MENCARI',
+        textTitle2: 'HUBUNGAN JANGKA PANJANG',
+        textQ1: 'APAKAH USIA ANDA LEBIH DARI 20 TAHUN?',
+        textQ2: 'APAKAH MENURUT ANDA, MENEMUKAN PASANGAN KENCAN DAPAT MEMBERI ANDA PENGALAMAN YANG MENYENANGKAN?',
+        textQ3: 'APAKAH ANDA INGIN BERTEMU GADIS YANG SERIUS, DAN MENGHABISKAN WAKTU YANG MENYENANGKAN BERSAMANYA DI SITUS KAMI?',
+        textThank: 'Terima kasih!',
+        textThankAfter: 'Anda sekarang dapat melihat daftar wanita kami yang ada di daerah Anda dan Anda dapat melihat foto-foto mereka. Kami bertanya lagi, tolong jaga kerahasiaan identitas mereka.',
+        textBtnk: 'Biarkan aku masuk!'
+    },
+    zh: {
+        textTitle1: '这些女人一直在寻找长期关系',
+        textTitle2: '',
+        textQ1: '您是否已经年满 20 岁？',
+        textQ2: '您是否认为寻找约会伙伴会是一次很好的体验？',
+        textQ3: '您是否想在我们的网站上结识一位认真的女孩并一起度过美好的时光？',
+        textThank: '谢谢！',
+        textThankAfter: '现在，您可以看到我们在您所在地区的女性名单，并且可以看到她们的照片。 我们会再次询问您，请保密您的身份',
+        textBtnk: '让我进入网站！'
+    },
+    it: {
+        textTitle1: 'QUESTE DONNE STANNO CERCANDO SOLO ',
+        textTitle2: 'RELAZIONI A LUNGO TERMINE',
+        textQ1: 'HAI PIÙ DI 20 ANNI?',
+        textQ2: 'PENSI CHE TROVARE UN PARTNER CON CUI USCIRE POTREBBE ESSERE UNA BELLA ESPERIENZA?',
+        textQ3: 'VUOI CONOSCERE UNA RAGAZZA SERIA E TRASCORRERE DEL TEMPO INSIEME SUL NOSTRO SITO?',
+        textThank: 'Grazie!',
+        textThankAfter: 'Adesso puoi visualizzare l\'elenco e le foto delle donne che si trovano nella tua zona. Ancora una volta, ti preghiamo di mantenere segreta la loro identità.',
+        textBtnk: 'Fammi entrare!'
+    },
+    nl: {
+        textTitle1: 'DEZE VROUWEN ZOEKEN ALLEEN',
+        textTitle2: 'LANGDURIGE RELATIES',
+        textQ1: 'BENT U OUDER DAN 20 JAAR?',
+        textQ2: 'DENK JE DAT HET VINDEN VAN EEN DATING PARTNER EEN GOEDE ERVARING KAN ZIJN?',
+        textQ3: 'WILT U EEN SERIEUS MEISJE ONTMOETEN EN SAMEN EEN GOEDE TIJD DOORBRENGEN OP ONZE SITE?',
+        textThank: 'Dank je wel!',
+        textThankAfter: 'U kunt nu onze lijst met vrouwen in uw omgeving zien en u kunt hun foto\'s zien. We vragen u nogmaals om de identiteit van de dems geheim te houden',
+        textBtnk: 'Laat me binnen!'
+    },
+    es: {
+        textTitle1: 'ESTAS MUJERES SOLO BUSCAN',
+        textTitle2: 'RELACIONES SERIAS',
+        textQ1: '¿TIENES MÁS DE 20 AÑOS?',
+        textQ2: '¿CREES QUE ENCONTRAR UNA COMPAÑERA DE CITAS PUEDE SER UNA BUENA EXPERIENCIA?',
+        textQ3: '¿TE GUSTARÍA CONOCER A UNA CHICA SERIA CON LA QUE PASARLO BIEN EN NUESTRO SITIO?',
+        textThank: '¡Gracias!',
+        textThankAfter: 'Ahora puedes ver nuestra lista de mujeres de tu zona y sus fotos. Te pedimos una vez más que mantengas su identidad en secreto.',
+        textBtnk: '¡Quiero acceder!'
+    },
+    fr: {
+        textTitle1: 'CES FEMMES CHERCHENT UNIQUEMENT',
+        textTitle2: 'RELATIONS À LONG TERME',
+        textQ1: 'AS TU PLUS DE 20 ANS ?',
+        textQ2: 'PENSES-TU QUE TROUVER UN PARTENAIRE DE RENCONTRE PEUT ÊTRE UNE BONNE EXPÉRIENCE ?',
+        textQ3: 'AIMERAIS-TU RENCONTRER UNE FILLE SÉRIEUSE ET PASSER DU BON TEMPS ENSEMBLE SUR NOTRE SITE ?',
+        textThank: 'Merci !',
+        textThankAfter: 'Vous pouvez maintenant voir notre liste et des photos de femmes qui sont dans votre région. Encore une fois, veuillez garder leur identité secrète. ',
+        textBtnk: 'Laissez-moi rentrer !'
+    },
+    ru: {
+        textTitle1: 'ЭТИ ЖЕНЩИНЫ ИЩУТ ТОЛЬКО',
+        textTitle2: 'ДЛИТЕЛЬНЫЕ ОТНОШЕНИЯ',
+        textQ1: 'ВАМ УЖЕ ЕСТЬ 20 ЛЕТ?',
+        textQ2: 'НАЙТИ СЕБЕ ПАРУ БУДЕТ ДЕЙСТВИТЕЛЬНО ХОРОШЕЙ ИДЕЕЙ ПО-ВАШЕМУ?',
+        textQ3: 'ВЫ ХОТЕЛИ БЫ НАЙТИ СЕРЬЁЗНУЮ ДЕВУШКУ, ЧТОБЫ ХОРОШО ПРОВОДИТЬ ВРЕМЯ В СИСТЕМЕ НАШЕГО САЙТА?',
+        textThank: 'Спасибо!',
+        textThankAfter: 'Теперь Вы можете видеть список и фотографии женщин, проживающих в вашей местности. Будьте добры, держите их личность в секрете.',
+        textBtnk: 'Позвольте войти!'
+    },
+    uk: {
+        textTitle1: 'ЦІ ЖІНКИ ШУКАЮТЬ ТІЛЬКИ',
+        textTitle2: 'ТРИВАЛИХ ВІДНОСИН',
+        textQ1: 'ВАМ ВЖЕ Є 20 РОКІВ?',
+        textQ2: 'ЧИ ДІЙСНО ВИ ВВАЖАЄТЕ, ЩО ШУКАТИ СОБІ ПАРУ - ЦЕ ДОБРА ДУМКА?',
+        textQ3: 'ЧИ ХОТІЛИ БИ ВИ ЗНАЙТИ СЕРЬЙОЗНУ ДІВЧИНУ, ЩОБ ДОБРЕ ПРОВОДИТИ ЧАС В СИСТЕМІ НАШОГО САЙТУ?',
+        textThank: 'Дякуємо!',
+        textThankAfter: 'Тепер Ви можете бачити список і фотографії жінок, які проживають у вашій місцевості. Будьте ласкаві, тримайте їх особистість в секреті.',
+        textBtnk: 'Дозвольте увійти!'
+    },
+    ro: {
+        textTitle1: 'Aceste femei sunt doar CAUTĂ',
+        textTitle2: 'RELATII PE TERMEN LUNG',
+        textQ1: 'Ai peste 20 de ani?',
+        textQ2: 'CREZI găsi un partener DATING poate fi o experiență bună?',
+        textQ3: 'V-ar dori să întâlni o fată și GRAVE SPEND TIMP BUN IMPREUNA PE SITE-UL NOSTRU?',
+        textThank: 'Mulțumesc!',
+        textThankAfter: 'Acum puteți vedea lista noastră de femei care sunt în zona dumneavoastră și puteți vedea imaginile lor. Vă întreb din nou vă rugăm să păstrați secretul identității DEM.',
+        textBtnk: 'Lasa-ma inauntru!'
+    },
+    vi: {
+        textTitle1: 'Những người phụ nữ chỉ tìm kiếm',
+        textTitle2: 'MỐI QUAN HỆ LÂU DÀI',
+        textQ1: 'LÀ BẠN hơn 20 năm?',
+        textQ2: 'Bạn có nghĩ rằng việc tìm kiếm một HẸN HÒ ĐỐI TÁC CÓ THỂ LÀ MỘT KINH NGHIỆM TỐT?',
+        textQ3: 'Bạn muốn gặp một cô gái nghiêm túc và tiêu SPEND TỐT LÚC NÀO CÙNG VỀ CHÚNG TÔI SITE?',
+        textThank: 'Cảm ơn bạn!',
+        textThankAfter: 'Bây giờ bạn có thể xem danh sách của chúng ta về những phụ nữ trong khu vực của bạn và bạn có thể xem hình ảnh của họ. Chúng tôi yêu cầu bạn một lần nữa xin vui lòng ghi DEMs bí mật danh tính.',
+        textBtnk: 'Cho tôi vào!'
+    },
+    cs: {
+        textTitle1: 'Tyto ženy jsou POUZE HLEDÁ',
+        textTitle2: 'Dlouhodobé vztahy',
+        textQ1: 'Jste starší 20 let?',
+        textQ2: 'MYSLÍTE FINDING datování partner může být dobrá zkušenost?',
+        textQ3: 'Chtěli byste se setkat s SERIOUS GIRL a strávit dobrý čas spolu na našich stránkách?',
+        textThank: 'Děkuji!',
+        textThankAfter: 'Nyní můžete vidět náš seznam žen, které jsou ve vaší oblasti a můžete vidět své obrázky. Žádáme vás zase mějte DEMS totožnost v tajnosti.',
+        textBtnk: 'Pusť mě dovnitř!'
+    },
+    sr: {
+        textTitle1: 'Ове жене су само ТРАЖИТЕ',
+        textTitle2: 'Дугорочне односе',
+        textQ1: 'ДА ЛИ СТЕ преко 20 година?',
+        textQ2: 'ДА ЛИ МИСЛИТЕ проналажење ЗНАКОМСТВА партнер може да буде добро искуство?',
+        textQ3: 'Да ли желите да упознате сериоус гирл и троше кад смо заједно на нашем сајту?',
+        textThank: 'Хвала вам!',
+        textThankAfter: 'Сада можете да видите нашу листу жена које су у својој области и да се виде слике. Ми вас опет имајте Демс идентитет у тајности.',
+        textBtnk: 'Пусти ме унутра!'
+    },
+    el: {
+        textTitle1: 'Αυτές οι γυναίκες είναι μόνο ψάχνει για',
+        textTitle2: 'Μακροχρόνιες σχέσεις',
+        textQ1: 'ΕΙΣΤΕ ΠΑΝΩ ΑΠΟ 20 ΧΡΟΝΙΑ;',
+        textQ2: 'DO YOU THINK εξεύρεση μιας εταίρος που χρονολογείται μπορεί να είναι ένας Εμπειρία Καλός;',
+        textQ3: 'Θέλετε να επιτύχει ένα σοβαρό GIRL και να περάσετε καλή στιγμή μαζί ΣΧΕΤΙΚΑ ΜΕ ΜΑΣ;',
+        textThank: 'Ευχαριστώ!',
+        textThankAfter: 'Τώρα μπορείτε να δείτε τη λίστα με τις γυναίκες που βρίσκονται στην περιοχή σας και μπορείτε να δείτε τις φωτογραφίες τους. Σας ζητάμε και πάλι να έχετε Δημοκρατικοί μυστική ταυτότητα.',
+        textBtnk: 'Ασε με να μπω!'
+    },
+    fa: {
+        textTitle1: 'این زنان فقط به دنبال روابط طولانی می گردند',
+        textTitle2: '',
+        textQ1: 'آیا شما همیشه 20 سال هستید؟',
+        textQ2: 'آیا شخصی شما واقعاً می توانید یک IDEA خوب در خودتان داشته باشید؟',
+        textQ3: 'آیا دوست دارید یک دختر جدی پیدا کنید تا یک زمان خوب در سیستم سایت ما داشته باشید؟',
+        textThank: '',
+        textThankAfter: 'ممنون اکنون می توانید لیست و عکس های زنانی که در منطقه شما زندگی می کنند را ببینید. لطفا هویت خود را مخفی نگه دارید.',
+        textBtnk: 'به من اجازه بده'
+    },
+    az: {
+        textTitle1: 'BU QADINLAR YALNIZ YOXDUR',
+        textTitle2: 'Uzunmüddətli Münasibətlər.',
+        textQ1: '20 İLDƏN VAR',
+        textQ2: 'SİZİN DƏSTƏK HƏRƏKƏTİNİN İSTİFADƏ OLUNMASINI ZİYARƏT ETMƏYİN?',
+        textQ3: 'Ciddi bir qızla tanış olmaq və saytımızda yaxşı bir vaxt keçirtmək istəyərdiniz?',
+        textThank: 'Çox sağ ol!',
+        textThankAfter: 'İndi ərazinizdə olan qadınlarımızın siyahısını görə bilərsiniz və şəkillərini görə bilərsiniz. Bir daha xahiş edirik, demlərin şəxsiyyətini gizli saxlayın.',
+        textBtnk: 'İçəri girin!'
+    },
+    hr: {
+        textTitle1: 'Te žene su samo u potrazi za',
+        textTitle2: 'Dugoročne odnose',
+        textQ1: 'JESTE LI preko 20 godina?',
+        textQ2: 'MISLITE pronalaženje PARTNERI partner može biti dobro iskustvo?',
+        textQ3: 'Želite li da zadovolji OZBILJNA DJEVOJKA i provesti dobro vrijeme zajedno na našim stranicama?',
+        textThank: 'Hvala vam!',
+        textThankAfter: 'Sada možete vidjeti naš popis žena koje su u svom području i možete vidjeti svoje slike. Molimo vas opet imajte DEM identitet u tajnosti.',
+        textBtnk: 'Pusti me unutra!'
+    },
+    hu: {
+        textTitle1: 'Ezek a nők csak keres',
+        textTitle2: 'Hosszú távú kapcsolatok',
+        textQ1: 'ÖN több mint 20 éve?',
+        textQ2: 'MIT GONDOLSZ találjanak társkereső partner lehet egy jó tapasztalat?',
+        textQ3: 'Szeretnél találkozni komoly lány, és töltsön jó ideje együtt oldalunkat?',
+        textThank: 'Köszönöm!',
+        textThankAfter: 'Most nézze meg a listát a nők, akik a területen, és láthatjuk a képeket. Arra kérünk benneteket ismét tartsa Dems kilétét.',
+        textBtnk: 'Engedj be!'
+    },
+    pl: {
+        textTitle1: 'KOBIETY SZUKAJĄ TYLKO ',
+        textTitle2: 'POWAŻNYCH ZWIĄZKÓW',
+        textQ1: 'MASZ WIĘCEJ NIŻ 20 LAT?',
+        textQ2: 'CZY MYŚLISZ, ŻE ZNALEZIENIE PARTNERA DO RANDKOWANIA MOŻE BYĆ DOBRYM DOŚWIADCZENIEM?',
+        textQ3: 'CZY CHCESZ SPOTKAĆ POWAŻNĄ DZIEWCZYNĘ I DOBRZE SPĘDZIĆ CZAS NA NASZEJ STRONIE?',
+        textThank: 'Dziękuję Ci!',
+        textThankAfter: 'Możesz teraz zobaczyć naszą listę i zdjęcia kobiet z Twojej okolicy. Ponownie, proszę, zachowaj ich tożsamość w tajemnicy.',
+        textBtnk: 'Wpuść mnie!'
+    },
+};
+
+
+$(document).ready(function () {
+    var langBrowser =
+    navigator.language || (navigator.languages && navigator.languages[0]),
+    param = langs[langBrowser];
+    if (param == undefined && langBrowser && langBrowser.length > 0) {
+        var parsedLang = langBrowser.slice(0, 2);
+        if (langs[parsedLang]) {
+            param = langs[parsedLang];
+        }
+    }
+
+    if (param == undefined) {
+        // required parameter
+        param = langs["en"];
+        // 'en' - default language, if you want change language, please select in object abbreviation. Example: 'fr', 'ru' etc.
+    }
+
+    $(".text-title-1").text(param.textTitle1);
+    $(".text-title-2").text(param.textTitle2);
+    $(".text-q-1").text(param.textQ1);
+    $(".text-q-2").text(param.textQ2);
+    $(".text-q-3").text(param.textQ3);
+    $(".text-thank").text(param.textThank);
+    $(".text-thank-after").text(param.textThankAfter);
+    $(".text-btn").text(param.textBtnk);
+});
